f93c77b50b3bced736dc029166b6e3984f6e4a4f
cuongnc <ncc.1988@gmail.com>
2024-05-07 00:15:20 +0700

    add

---
+++ 02.jenkins/scripts/jenkins-init.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# volume setup
+vgchange -ay
+
+DEVICE_FS=`blkid -o value -s TYPE ${DEVICE}`
+if [ "`echo -n $DEVICE_FS`" == "" ] ; then 
+  # wait for the device to be attached
+  DEVICENAME=`echo "${DEVICE}" | awk -F '/' '{print $3}'`
+  DEVICEEXISTS=''
+  while [[ -z $DEVICEEXISTS ]]; do
+    echo "checking $DEVICENAME"
+    DEVICEEXISTS=`lsblk |grep "$DEVICENAME" |wc -l`
+    if [[ $DEVICEEXISTS != "1" ]]; then
+      sleep 15
+    fi
+  done
+  pvcreate ${DEVICE}
+  vgcreate data ${DEVICE}
+  lvcreate --name volume1 -l 100%FREE data
+  mkfs.ext4 /dev/data/volume1
+fi
+mkdir -p /var/lib/jenkins
+echo '/dev/data/volume1 /var/lib/jenkins ext4 defaults 0 0' >> /etc/fstab
+mount /var/lib/jenkins
+
+
+# install dependencies
+yum install -y python3 openjdk-11-jdk awscli
+# install jenkins
+yum install -y unzip
+wget https://get.jenkins.io/redhat/jenkins-2.444-1.1.noarch.rpm
+yum install -y jenkins-2.444-1.1.noarch.rpm
+
+
+# install terraform
+wget -q https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
+&& unzip -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin \
+&& rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+
+# install packer
+cd /usr/local/bin
+wget -q https://releases.hashicorp.com/packer/0.10.2/packer_0.10.2_linux_amd64.zip
+unzip packer_0.10.2_linux_amd64.zip
+# clean up
+# apt-get clean
+rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+rm packer_0.10.2_linux_amd64.zip
